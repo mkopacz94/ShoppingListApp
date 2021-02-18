58d9b2c9840df82551e5c674266d9971a2388d6c
mkopacz94 <47296347+mkopacz94@users.noreply.github.com>
2021-02-18 22:20:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.shoppinglistapp
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(applicationContext, R.drawable.toolbar_gradient));
+    }
+}
